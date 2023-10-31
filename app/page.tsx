bfae189b1af00e75992d165192b37793d7173bc8
PhantomAnything <133027840+PhantomSaifIT@users.noreply.github.com>
2023-10-31 07:14:11 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Encryption from "@/components/main/Encryption";
+import Hero from "@/components/main/Hero";
+import Skills from "@/components/main/Skills";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
+        <Hero />
+        <Skills />
+        <Encryption />
+      </div>
+    </main>
+  );
+}
